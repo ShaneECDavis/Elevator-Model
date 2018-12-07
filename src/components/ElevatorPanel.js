462b11c447fd6b02d85f9c9e88c1f6f71daffbe8
Shane EC Davis <shaneecdavis7@gmail.com>
2018-12-07 06:20:21 -0600

    initial commit

---
+++ src/components/ElevatorPanel.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+
+const ElevatorPanel = (props) =>{
+  return <h1> ElevatorPanel </h1>
+}
+
+export default ElevatorPanel
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+
+export {default as ElevatorPanel} from './ElevatorPanel'
